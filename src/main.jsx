c0cbbd1f6c679c17ab1b4a09431ed2e8c711a8ef
Nicholas Winkler <nhwinkler@gmail.com>
2024-10-23 00:54:24 -0500

    Added main.jsx and structured project files

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home"; // Import the Home component
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Home />); // Render the Home component inside the root div
